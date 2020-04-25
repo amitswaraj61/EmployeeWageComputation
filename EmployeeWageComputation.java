d998c2a269bac1d77091b73ab3741f2240897606
amitswaraj61 <aswaraj78@gmail.com>
2020-04-25 13:25:01 +0530

    Added Employee Attendence Feature in empWageComputation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,6 +5,13 @@ class EmployeeWageComputation
     public static void main(String args[])
     {
         System.out.println("Welcome to Employee Wage Computation");
+        int isPresent=1;
+        int randomValue=(int) ( Math.random() * 2 + 1);
+        if ((int) ( Math.random() * 2 + 1) ==isPresent) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 
 }
