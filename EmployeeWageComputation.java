183b5286fd4b1eff1343bd986897d6cf554e8708
amitswaraj61 <aswaraj78@gmail.com>
2020-04-25 13:48:32 +0530

    Added Daily Wage Feature for employeeWageComputation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,17 +1,19 @@
 package com.emp;
 
-class EmployeeWageComputation
-{
-    public static void main(String args[])
-    {
+class EmployeeWageComputation {
+    public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
-        int isPresent=1;
-        int randomValue=(int) ( Math.random() * 2 + 1);
-        if ((int) ( Math.random() * 2 + 1) ==isPresent) {
-            System.out.println("Employee is present");
+        int isPresent = 1;
+        int empWagePerHr = 20;
+        int empHrs = 8;
+        int salary=0;
+        int randomValue = (int) (Math.random() * 2 + 1);
+        if ((int) (Math.random() * 2 + 1) == isPresent) {
+            System.out.println("salary is"+" "+empWagePerHr * empHrs);
         } else {
-            System.out.println("Employee is absent");
+            salary=0;
+             System.out.println("Employee is absent"+" "+salary);
         }
-    }
 
+    }
 }
