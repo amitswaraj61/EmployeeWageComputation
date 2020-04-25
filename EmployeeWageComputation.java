283c063bef5a1b491a7f7eae0f8e3f4c5ec3cd71
amitswaraj61 <aswaraj78@gmail.com>
2020-04-25 14:02:57 +0530

    Added Add Part Time Feature for employeeWageComputation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,16 +4,22 @@ class EmployeeWageComputation {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
         int isPresent = 1;
+        int ispartTime = 1;
+        int isfullTime = 2;
         int empWagePerHr = 20;
-        int empHrs = 8;
-        int salary=0;
+        int salary = 0;
+        int empHrs = 0;
         int randomValue = (int) (Math.random() * 2 + 1);
-        if ((int) (Math.random() * 2 + 1) == isPresent) {
-            System.out.println("salary is"+" "+empWagePerHr * empHrs);
+        if (randomValue == ispartTime) {
+            empHrs = 4;
+        } else if (randomValue == isfullTime) {
+            empHrs = 8;
         } else {
-            salary=0;
-             System.out.println("Employee is absent"+" "+salary);
+            empHrs = 0;
         }
+
+        salary = (empWagePerHr * empHrs);
+        System.out.println(salary);
 
     }
 }
