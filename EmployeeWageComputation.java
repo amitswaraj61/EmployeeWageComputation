228266f3221971b08add8aabe9ea5ea283dbf632
amitswaraj61 <aswaraj78@gmail.com>
2020-04-25 14:19:56 +0530

    Added Monthly Wage Calculation for employeeWageComputation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,23 +3,27 @@ package com.emp;
 class EmployeeWageComputation {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
-        int ispartTime = 1;
-        int isfullTime = 2;
-        int empWagePerHr = 20;
+        final int ispartTime = 1;
+        final int isfullTime = 2;
+        final int empWagePerHr = 20;
+        final int workingDays = 20;
         int salary = 0;
         int empHrs = 0;
-        int randomValue = (int) (Math.random() * 3 + 1);
-        switch (randomValue) {
-            case 1:
-                empHrs = 4;
-                break;
-            case 2:
-                empHrs = 8;
-                break;
-            case 3:
-                empHrs = 0;
+        int monthlySalary = 0;
+        for (int i = 1; i <= workingDays; i++) {
+            int randomValue = (int) (Math.random() * 3 + 1);
+            switch (randomValue) {
+                case 1:
+                    empHrs = 4;
+                    break;
+                case 2:
+                    empHrs = 8;
+                    break;
+                case 3:
+                    empHrs = 0;
+            }
+            salary = empWagePerHr * empHrs;
+            System.out.println(salary);
         }
-        salary = empWagePerHr * empHrs;
-        System.out.println(salary);
     }
 }
