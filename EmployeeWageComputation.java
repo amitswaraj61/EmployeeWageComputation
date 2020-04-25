cfe1e4ba5db039ae7c5e02339e0648d898de0b31
amitswaraj61 <aswaraj78@gmail.com>
2020-04-25 14:11:21 +0530

    Added Switch Statement Solution for employeeWageComputation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,23 +3,23 @@ package com.emp;
 class EmployeeWageComputation {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation");
-        int isPresent = 1;
         int ispartTime = 1;
         int isfullTime = 2;
         int empWagePerHr = 20;
         int salary = 0;
         int empHrs = 0;
-        int randomValue = (int) (Math.random() * 2 + 1);
-        if (randomValue == ispartTime) {
-            empHrs = 4;
-        } else if (randomValue == isfullTime) {
-            empHrs = 8;
-        } else {
-            empHrs = 0;
+        int randomValue = (int) (Math.random() * 3 + 1);
+        switch (randomValue) {
+            case 1:
+                empHrs = 4;
+                break;
+            case 2:
+                empHrs = 8;
+                break;
+            case 3:
+                empHrs = 0;
         }
-
-        salary = (empWagePerHr * empHrs);
+        salary = empWagePerHr * empHrs;
         System.out.println(salary);
-
     }
 }
