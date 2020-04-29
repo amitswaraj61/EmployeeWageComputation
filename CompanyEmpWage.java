baa2f6f6a427ad9d4be8e74a20a8cc3dd0a72930
amitswaraj61 <aswaraj78@gmail.com>
2020-04-29 20:10:18 +0530

    Using Array Manage Multiple Company for employeeWageComputation

---
+++ CompanyEmpWage.java
@@ -0,0 +1,51 @@
+package com.emp;
+
+public class CompanyEmpWage
+{
+    public static int empHrs = 0;
+    //Instance variables
+    public final String companyName;
+    public final int empRate;
+    public final int numOfDays;
+    public final int maxHrs;
+
+    public CompanyEmpWage(String companyName, int empRate, int numOfDays, int maxHrs) {
+        this.companyName = companyName;
+        this.empRate = empRate;
+        this.numOfDays = numOfDays;
+        this.maxHrs = maxHrs;
+    }
+
+
+    public static int getWorkingHours(int randomValue) {
+
+        switch (randomValue) {
+            case 1:
+                empHrs = 4;
+                break;
+            case 2:
+                empHrs = 8;
+                break;
+            case 3:
+                empHrs = 0;
+        }
+        return empHrs;
+    }
+
+    public  void calculateEmployeeWageForCompany() {
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        int totalWage = 0;
+        while (totalEmpHrs < maxHrs && totalWorkingDays < numOfDays) {
+            totalWorkingDays++;
+            int randomValue = (int) (Math.random() * 3 + 1);
+            empHrs = getWorkingHours(randomValue);
+            //totalEmpHrs += empHrs;
+            int monthlySalary = empRate * empHrs;
+            totalWage += monthlySalary;
+           // System.out.println(monthlySalary);
+        }
+        System.out.println("Total Employee wage For Company" + " " + companyName + " " + totalWage);
+    }
+
+}
---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,30 @@
+package com.emp;
+
+import java.util.Scanner;
+
+
+class EmployeeWageBuilder {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Employee Wage Computation");
+        CompanyEmpWage[] companyEmpWage=new CompanyEmpWage[10];
+        for (int index =0; index <companyEmpWage.length ; index++) {
+            System.out.println("Please enter the" + index + "st company deatils");
+            System.out.println("enter the company name");
+            String companyName = scanner.nextLine();
+            System.out.println("enter the rate per hour");
+            int empRate = scanner.nextInt();
+            System.out.println("enter the number of days");
+            int numOfDays = scanner.nextInt();
+            System.out.println("enter the maximum working hrs in a month");
+            int maxHrs = scanner.nextInt();
+            scanner.nextLine();
+        companyEmpWage[index] =new CompanyEmpWage(companyName, empRate, numOfDays, maxHrs);
+        }
+        for (CompanyEmpWage companyEmpWage1:companyEmpWage) {
+            companyEmpWage1.calculateEmployeeWageForCompany();
+        }
+
+    }
+}
+
